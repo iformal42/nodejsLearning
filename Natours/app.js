acc659449e5186aa0a5b4954d3820cec1f00ec57
musaif bangi <musaif.bangi@ncircletech.com>
2026-05-12 12:19:01 +0530

    static file implemented

--- Natours/app.js
+++ Natours/app.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
-
+app.use(express.static('./public'));
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
