768c7ba7a11339af0f793e2b1beb676a551d80ad
musaif bangi <musaif.bangi@ncircletech.com>
2026-05-15 12:19:52 +0530

    eslint setup

--- Natours/app.js
+++ Natours/app.js
@@ -1,19 +1,13 @@
-const fs = require('fs');
 const express = require('express');
 
 const morgan = require('morgan');
-const {
-  getAllUsers,
-  createUser,
-  getUser,
-  updateUser,
-  deleteUser,
-} = require('./controller/userController');
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
-const app = express();
 
-app.use(morgan('dev'));
+const app = express();
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
 app.use(express.json());
 app.use(express.static('./public'));
 app.use('/api/v1/tours', tourRouter);
--- Natours/server.js
+++ Natours/server.js
@@ -1,6 +1,8 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './.env' });
 const app = require('./app');
 
-const port = 3000;
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Listening on port: ${port}...`);
 });
