8d09a39809a47712016e3384799ef826c611a592
musaif bangi <musaif.bangi@ncircletech.com>
2026-05-27 18:11:27 +0530

    implemented validators

--- Natours/models/tourModel.js
+++ Natours/models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -10,6 +11,7 @@ const tourSchema = new mongoose.Schema(
       trim: true,
       maxlength: [40, 'A tour name must have max 40 chars'],
       minlength: [10, 'A tour name must have min 10 chars'],
+      // validate: [validator.isAlpha, 'Name should only contain character'],
     },
     slug: String,
     duration: {
@@ -42,7 +44,16 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, 'A tour must have a price'],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          // this only points to current doc on NEW Document
+          return this.price > val;
+        },
+        message: 'Discount price ({VALUE}) should be less than the price',
+      },
+    },
     summary: {
       type: String,
       trim: true,
