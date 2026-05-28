1ceabd172a7a3339a593c201f3f8430b7563f415
musaif bangi <musaif.bangi@ncircletech.com>
2026-05-28 19:05:53 +0530

    unhandled unknown route

--- Natours/app.js
+++ Natours/app.js
@@ -5,11 +5,22 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
+
+const baseUrl = '/api/v1';
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
 app.use(express.json());
 app.use(express.static('./public'));
-app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/users', userRouter);
 
+app.use(`${baseUrl}/tour`, tourRouter);
+app.use(`${baseUrl}/users`, userRouter);
+
+// This runs if above middle ware wont triggered
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'failed',
+    message: `Cant find ${req.originalUrl} on this server`,
+  });
+  next();
+});
 module.exports = app;
